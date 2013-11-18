912527f46ff7210b45f291eb6f018de3fa3b1c65
UncleBill <billbill290@gmail.com>
2013-11-18 17:10:23 +0800

    desktop done

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+    var $subListCons = $(".sub-list-con");
+    $subListCons.bind('hover click', function () {
+        $(this)
+            .siblings()
+            .removeClass('show')
+        .end()
+            .addClass('show');
+    });
+    $(".select-btn").bind('click', function () {
+        $(".team-select").toggleClass('select-active');
+    })
+})(jQuery);
